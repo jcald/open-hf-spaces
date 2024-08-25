4cf128329707885036e959429c920744c6142877
José Calderón <josecalderoncelis@gmail.com>
2024-08-25 04:08:21 +0000

    Descripción de tus cambios

--- wrk/multimodalart--flux-lora-the-explorer/random.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-curl -X POST https://multimodalart-flux-lora-the-explorer.hf.space/call/get_random_value -s -H "Content-Type: application/json" -d '{
-  "data": [
-]}' \
-  | awk -F'"' '{ print $4}'  \
-  | read EVENT_ID; curl -N https://multimodalart-flux-lora-the-explorer.hf.space/call/get_random_value/$EVENT_ID
--- wrk/multimodalart--flux-lora-the-explorer/upd.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-curl -X POST https://multimodalart-flux-lora-the-explorer.hf.space/call/update_selection -s -H "Content-Type: application/json" -d '{
-  "data": [
-    256,
-    256
-]}' \
-  | awk -F'"' '{ print $4}'  \
-  | read EVENT_ID; curl -N https://multimodalart-flux-lora-the-explorer.hf.space/call/update_selection/$EVENT_ID
