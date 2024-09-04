46c4a55d49377b5b3a0a34e4928cfef425627919
José Calderón <josecalderoncelis@gmail.com>
2024-09-04 07:08:06 +0000

    Descripción de tus cambios

--- wrk/ali-vilab--text-to-video-ms-1.7b/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#pip install diffusers transformers accelerate torch
-
--- wrk/ali-vilab--text-to-video-ms-1.7b/installLong.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#pip install git+https://github.com/huggingface/diffusers transformers accelerate
-
